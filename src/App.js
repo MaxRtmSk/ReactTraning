7bc54d6640f22d9e5990d76c0b41398e0d1715b1
Max <max.work.dev@gmail.com>
2021-01-20 20:10:59 +0300

    delete button

--- src/App.js
+++ src/App.js
@@ -5,95 +5,14 @@ import { render } from "@testing-library/react";
 import ReactDOM from "react-dom";
 import { SignUp } from "./SingUp";
 
-class SocialButton extends React.Component {
-  render() {
-    return (
-      <>
-        <a href="#" className={`fa fa-${this.props.type}`}></a>
-      </>
-    );
-  }
-}
-
-class TwitterButton extends React.Component {
-  render() {
-    return <SocialButton type="twitter" />;
-  }
-}
-
-class FacebookButton extends React.Component {
-  render() {
-    return <SocialButton type="facebook" />;
-  }
-}
-
-class IconBar extends React.Component {
-  render() {
-    return (
-      <div className={this.props.orientation}>
-        <div className="icon-bar">
-          <a className="active" href="#">
-            <i className="fa fa-home"></i>
-          </a>
-          <a href="#">
-            <i className="fa fa-search"></i>
-          </a>
-          <a href="#">
-            <i className="fa fa-envelope"></i>
-          </a>
-          <a href="#">
-            <i className="fa fa-globe"></i>
-          </a>
-          <a href="#">
-            <i className="fa fa-trash"></i>
-          </a>
-        </div>
-      </div>
-    );
-  }
-}
-
-class Navbar extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      activeItem: this.props.initialActiveItemId,
-      arrayItems: this.props.items,
-    };
-  }
-
-  render() {
-    return (
-      <div className="navbar">
-        {this.state.arrayItems.map((item) => {
-          let active = null
-          if (item === this.state.activeItem){
-            active = 'active'
-          } 
-          return (
-            <a className={active} href="#">
-              <i className="fa fa-fw"></i>
-              {item}
-            </a>
-          );
-        })}
-      </div>
-    );
-  }
-}
 
 function App() {
   return (
     <div className="App">
       <SignUp onSignUp = {(e) => {
         console.log(e.login)
-      }}/>
-      <TwitterButton />
-      <FacebookButton />
-      <IconBar orientation="vertical" />
-      <IconBar orientation="horizontal" />
-      <Navbar items={["Home", "Search", "About"]} initialActiveItemId="Search" />
-      
+        console.log(e.password)
+      }}/> 
     </div>
   );
 }
--- src/SingUp.js
+++ src/SingUp.js
@@ -1,33 +1,90 @@
-import { render } from '@testing-library/react';
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from "@testing-library/react";
+import React from "react";
+import ReactDOM from "react-dom";
 
 export class SignUp extends React.Component {
-  constructor(psops){
-    super(psops)
+  constructor(psops) {
+    super(psops);
     this.state = {
-      login : '',
-      password : '',
-      passwordConfirmation: ''
-    }
+      login: "",
+      password: "",
+      passwordConfirmation: "",
+      passwordError: false,
+      loginError: false,
+      passwordConfirmationError: false,
+    };
   }
 
-  render(){
-    return <form onSubmit = {(e) => {e.preventDefault()}}>
-    <input 
-    name = 'login'
-    type = 'text'
-    value = {this.state.login}
-    onChange = {(e) => {
-      this.setState({
-        login: e.target.value
-      })
-    }}/>
-    <button
-      onClick={()=>{
-        this.props.onSignUp({login: this.state.login})
-      }}
-    >Submit</button>
-    </form>
+  render() {
+    return (
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+      >
+        <input
+          name="login"
+          type="text"
+          value={this.state.login}
+          onChange={(e) => {
+            this.setState({
+              login: e.target.value,
+            });
+          }}
+        />
+        <input
+          name="password"
+          type="password"
+          value={this.state.password}
+          onChange={(e) => {
+            this.setState({
+              password: e.target.value,
+            });
+          }}
+        />
+        <input
+          name="passwordConfirmation"
+          type="password"
+          value={this.state.passwordConfirmation}
+          onChange={(e) => {
+            this.setState({
+              passwordConfirmation: e.target.value,
+            });
+          }}
+        />
+        <button
+          onClick={() => {
+            const nextState = {};
+            let hasError = false;
+
+            if (this.state.login.trim() === "") {
+              nextState.loginError = true;
+              hasError = true;
+            }
+
+            if (this.state.password === "") {
+              nextState.passwordError = true;
+              hasError = true;
+            }
+
+            if (this.state.passwordConfirmation === "") {
+              nextState.passwordConfirmationError = true;
+              hasError = true;
+            }
+
+            if (hasError) {
+              this.setState(nextState);
+            } else {
+              this.props.onSignUp({
+                login: this.state.login,
+                password: this.state.password,
+              });
+            }
+          }}
+        >
+          Submit
+        </button>
+      </form>
+    );
   }
 }
