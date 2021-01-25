9cfa43f1fa426fdadbda1ae00078aec20261e921
Max <max.work.dev@gmail.com>
2021-01-25 22:07:14 +0300

    added input number validation

---
+++ src/numberValidation.js
@@ -0,0 +1,35 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./numberValidation.css";
+
+export class NumberValidation extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      number: "",
+      error: false,
+    };
+  }
+
+  render() {
+    return (
+      <>
+        <input
+          type="text"
+          value={this.state.number}
+          maxLength="13"
+          className={this.state.error ? "numberfalied" : ""}
+          onChange={(e) => {
+            let number = e.target.value.substr(0, 6)
+            console.log(number)
+            if (number == '+37529' || number == '+37533' || number == '+37544' || number == '+37525') {
+              this.setState({ number: this.value, error: false });
+            } else {
+              this.setState({ number: this.value, error: true });
+            }
+          }}
+        />
+      </>
+    );
+  }
+}
